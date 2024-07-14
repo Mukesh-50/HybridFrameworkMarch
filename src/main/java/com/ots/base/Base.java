d7d8f2fa4e096b61d555071bd34c3d20453811a2
Mukesh Otwani <mukeshotwani.50@gmail.com>
2024-07-14 22:09:55 +0530

    Update Base.java

--- src/main/java/com/ots/base/Base.java
+++ src/main/java/com/ots/base/Base.java
@@ -14,15 +14,7 @@ public class Base {
 	@BeforeMethod
 	public void setup()
 	{
-
-		System.out.println("LOG:INFO - Driver Value Before "+driver);
-		
-		System.out.println("LOG:INFO - Starting Browser - Running Before Method ");
-		
 		driver=BrowserFactory.startBrowser(ConfigReader.getProperty("browserName"), ConfigReader.getProperty("appURLQA")+"/login");
-		
-		System.out.println("LOG:INFO - Browser and Application up and running ");
-		
 	}
 	
 	@AfterMethod
